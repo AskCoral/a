cc83fe4627afdf1aeb428811f85d031784be7adb
Will Stone <654103+will-stone@users.noreply.github.com>
2018-10-10 14:27:28 +0100

    fix: action creator params are not optional

--- src/a.ts
+++ src/a.ts
@@ -1,16 +1,20 @@
-interface Action<T = any> {
+interface Action<T = string> {
   type: T
 }
 
 type PayloadFn<Obj extends {}, R> = (obj: Obj) => R
+
 interface ActionCreator<T, Obj extends {}, A> {
   TYPE: T
-  (obj: Obj): A
+  (obj?: Obj): A
 }
 
 type TAction<T extends string, Payloads extends {} = {}> = Action<T> &
   { [K in keyof Payloads]: Payloads[K] }
 
+/**
+ * a
+ */
 export function a<T extends string>(type: T): ActionCreator<T, {}, TAction<T>>
 export function a<T extends string, Obj extends {}, R>(
   type: T,
@@ -28,6 +32,9 @@ export function a(type: string, payloadFn?: (obj: object) => object) {
   return actionCreator
 }
 
+/**
+ * p
+ */
 export function p<Obj extends {}>(): (obj: Obj) => Obj
 export function p() {
   return (obj: object) => obj
