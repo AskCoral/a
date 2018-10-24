9db8fbd6af154aab2e98a6a10e329ade67f14dd8
Will Stone <654103+will-stone@users.noreply.github.com>
2018-10-24 22:53:12 +0100

    wip feat: p-less

--- demo/actions.ts
+++ demo/actions.ts
@@ -1,12 +1,12 @@
-import { a, p } from '../src/a'
+import { a } from '../src/a'
 
-const ATestAction = a('A_TEST_ACTION', p<{ str: string }>())
+const ATestAction = a<{ str: string }>('A_TEST_ACTION')
 type ATestAction = ReturnType<typeof ATestAction>
 
 const ASecondTestAction = a('A_SECOND_TEST_ACTION')
 type ASecondTestAction = ReturnType<typeof ASecondTestAction>
 
-const AThirdTestAction = a('A_THIRD_TEST_ACTION', p<{ num: number }>())
+const AThirdTestAction = a<{ num: number }>('A_THIRD_TEST_ACTION')
 type AThirdTestAction = ReturnType<typeof AThirdTestAction>
 
 export { ATestAction, ASecondTestAction, AThirdTestAction }
--- src/a.ts
+++ src/a.ts
@@ -1,48 +1,12 @@
-type TPayloadFn<Obj extends {}, R> = (obj: Obj) => R
-
-interface ISimpleA<T, Ac> {
-  TYPE: T
-  (): Ac
-}
-
-interface IPayloadA<T, Obj extends {}, Ac> {
-  TYPE: T
-  (obj: Obj): Ac
-}
-
-type TSimpleAction<T extends string> = {
-  type: T
-}
-
-type TPayloadAction<T extends string, Payloads extends {} = {}> = TSimpleAction<
-  T
-> &
-  { [K in keyof Payloads]: Payloads[K] }
-
-/**
- * a
- */
-export function a<T extends string>(type: T): ISimpleA<T, TSimpleAction<T>>
-export function a<T extends string, Obj extends {}, R>(
-  type: T,
-  fn: TPayloadFn<Obj, R>,
-): IPayloadA<T, Obj, TPayloadAction<T, R>>
-export function a(type: string, payloadFn?: (obj: object) => object) {
-  const actionCreator: any = (obj: object) => {
-    if (payloadFn) {
-      const extra = payloadFn(obj)
-      return { type, ...extra }
-    }
-    return { type }
+export function a<Obj extends {} | undefined = undefined>(
+  type: string,
+): {
+  TYPE: string
+  (obj?: Obj): { type: string } & { [K in keyof Obj]: Obj[K] }
+} {
+  const actionCreator: any = (obj: Obj) => {
+    return Object.assign({ type }, obj)
   }
   actionCreator.TYPE = type
   return actionCreator
-}
-
-/**
- * p
- */
-export function p<Obj extends {}>(): (obj: Obj) => Obj
-export function p() {
-  return (obj: object) => obj
 }
